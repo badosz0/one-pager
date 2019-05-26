cabbb8d3a8f9d3e014e0bc081280315c15f22c35
badosz0 <badoszk@gmail.com>
2019-05-26 12:12:32 +0200

    clear

--- createimage.py
+++ createimage.py
@@ -5,6 +5,8 @@ from eta import ETA
 width = 4960
 height = 7016
 fontIn = 'fonts/Roboto-Light.ttf'
+margin = 40
+offset = 40
 
 def textwrap(text, width):
     return [text[i:i+width] for i in range(0, len(text), width)]
@@ -15,19 +17,14 @@ else:
     text = open(sys.argv[1],"r", encoding="utf-8").read().replace("\n"," ").replace("\t"," ")
 
     blank = Image.new('RGBA', (width, height), color=(255, 255, 255))
-
     font = ImageFont.truetype(font=fontIn, size=11)
 
     canvas = ImageDraw.Draw(blank)
 
-    margin = offset = 40
-
-    curline = 0
-
-    text = textwrap(text, 900)
+    text = textwrap(text, 950)
     lines = len(text)
 
-    eta = ETA(lines)
+    eta = ETA(lines/10)
     for line in text:
         canvas.text((margin, offset), line, font=font, fill="#000000")
         offset += font.getsize(line)[1]
