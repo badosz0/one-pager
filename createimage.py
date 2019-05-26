c2815214526746c0aa3a73ce15a411a581e6312c
badosz0 <badoszk@gmail.com>
2019-05-26 11:05:26 +0200

    text file from args

--- createimage.py
+++ createimage.py
@@ -1,18 +1,37 @@
 from PIL import Image, ImageDraw, ImageFont
-import textwrap
-file = open("tadeusz.txt","r", encoding="utf-8") 
-text = file.read()
-text = text.replace("\n"," ")
-file.close()
-blank = Image.new('RGBA', (4960, 7016), color=(255, 255, 255))
-font = ImageFont.truetype(font='Roboto-Light.ttf', size=11)
-canv = ImageDraw.Draw(blank)
-margin = offset = 40
-cur = 0
-lines = len(textwrap.wrap(text, width=950))
-for line in textwrap.wrap(text, width=950):
-    cur = cur + 1
-    canv.text((margin, offset), line, font=font, fill="#000000")
-    offset += font.getsize(line)[1]
-    print(round(cur/lines*100,2),"%")
-blank.save('done.png')
+import textwrap, sys
+
+if len(sys.argv) < 2 or sys.argv[1][-4:] != '.txt':
+    print('Please specify .txt file.')
+else:
+    file = open(sys.argv[1],"r", encoding="utf-8")
+
+    text = file.read()
+
+    text = text.replace("\n"," ")
+
+    file.close()
+
+    blank = Image.new('RGBA', (4960, 7016), color=(255, 255, 255))
+
+    font = ImageFont.truetype(font='Roboto-Light.ttf', size=11)
+
+    canv = ImageDraw.Draw(blank)
+
+    margin = offset = 40
+
+    cur = 0
+
+    lines = len(textwrap.wrap(text, width=950))
+
+    for line in textwrap.wrap(text, width=950):
+
+        cur = cur + 1
+
+        canv.text((margin, offset), line, font=font, fill="#000000")
+
+        offset += font.getsize(line)[1]
+
+        print(round(cur/lines*100,2),"%")
+
+    blank.save('done.png')
